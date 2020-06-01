913707e061694c1be645243e536c9de19d7d17ad
asbrant117 <46379990+asbrant117@users.noreply.github.com>
2020-06-02 00:28:15 +0300

    Добавили отдачу полей

--- app/controllers/JsonController.scala
+++ app/controllers/JsonController.scala
@@ -1,22 +1,35 @@
 package controllers
 
-import db.{BoardgameRepository, CountryType, GenreRepository, UserProfile, UserProfileRepository}
+import db.{Boardgame, BoardgameRepository, CountryRepository, CountryType, GenreClass, GenreRepository, UserProfile, UserProfileRepository}
 import javax.inject._
 import play.api.mvc._
 import play.api.libs.json.{JsPath, JsValue, Json, Reads, Writes}
 import play.api.libs.functional.syntax._
 
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.Duration
+
 @Singleton
 class JsonController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
   val repoBG = new BoardgameRepository
   val repoG = new GenreRepository
-
+  val repoC = new CountryRepository
 
   implicit val boardgameRead: Reads[BoardgameJson] = Json.reads[BoardgameJson]
   implicit val boardgameWrites: Writes[BoardgameJson] = Json.writes[BoardgameJson]
 
   def boardgame(id: Int): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    val boardgame = BoardgameJson(id, "genre1", "name1", "country1", 3)
+    val gameFuture = repoBG.get(id)
+    val game: Boardgame = Await.result(gameFuture, Duration.Inf).get
+
+    val genreFuture: Future[Option[GenreClass]] = repoG.get(game.genreId)
+    val genre: Option[GenreClass] = Await.result(genreFuture, Duration.Inf)
+
+    val countryFuture = repoC.get(game.countryId)
+    val country = Await.result(countryFuture, Duration.Inf).get.country
+
+    val boardgame = BoardgameJson(id, genre.get.genre, name = game.name, country = country, rating = game.rating)
+
     Ok(Json.toJson(boardgame))
   }
 
